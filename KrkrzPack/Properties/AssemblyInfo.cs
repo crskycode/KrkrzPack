316e7f6ba991be02af6154fb330f55cf1a0a369a
crskycode <crskycode@hotmail.com>
2021-02-28 17:54:43 +0800

    Added assembly info

--- KrkrzPack/Properties/AssemblyInfo.cs
+++ KrkrzPack/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("KrkrzPack")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Create archive file for KirikiriZ engine.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("CrskyCode")]
 [assembly: AssemblyProduct("KrkrzPack")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  2021 CrskyCode")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
